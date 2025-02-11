0cdd54902759494cc27dbdf7bfb5783a71eb0525
Famez1 <a.plyakin@sh.dunice.net>
2025-02-11 21:33:14 +0300

    Добавлен DbContext и начальная его конфигурация

---
+++ src/JsonParser.Abstractions/Persistence/Base/BaseDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace JsonParser.Abstractions.Persistence.Base;
+
+public abstract class BaseDbContext : DbContext
+{
+    protected BaseDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    protected sealed override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+        ConfigureModelBuilder(modelBuilder);
+    }
+
+    protected virtual void ConfigureModelBuilder(ModelBuilder modelBuilder)
+    {
+    }
+}
---
+++ src/JsonParser.Persistence/ParseJsonDbContext.cs
@@ -0,0 +1,20 @@
+﻿using JsonParser.Abstractions.Persistence.Base;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace JsonParser.Persistence;
+
+public class ParseJsonDbContext : BaseDbContext
+{
+    public ParseJsonDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+
+    protected override void ConfigureModelBuilder(ModelBuilder modelBuilder)
+    {
+        base.ConfigureModelBuilder(modelBuilder);
+
+        modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+    }
+}
